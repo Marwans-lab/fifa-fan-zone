b56882a51b677120498b021c2306ef979464cb33
Marwans-lab <abderazak.boudiaf@gmail.com>
2026-03-31 18:12:38 +0300

    Revert "MAR-228: Angular migration scaffolding and build setup (#212)"
    This reverts commit 1fe56fb6a00bc3784fed71130265dbdea0f26e1b.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
-import angular from '@analogjs/vite-plugin-angular'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  resolve: {
-    mainFields: ['module'],
-  },
-  plugins: [angular(), react()],
+  plugins: [react()],
   base: '/fifa-fan-zone/',
   optimizeDeps: {
     exclude: ['onnxruntime-web'],
