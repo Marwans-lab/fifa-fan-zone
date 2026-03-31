5f7b437e4dbab993ec828c23b0deb0cd21e4c873
Marwans-lab <abderazak.boudiaf@gmail.com>
2026-03-31 22:58:23 +0300

    Revert "MAR-230: Add Angular auth, Firebase, QAApp, and analytics services (#220)"
    This reverts commit 114041d00a67c8c846e15949547945460b7053ff.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './guards/auth.guard';
 
 export const appRoutes: Routes = [
   {
     path: '',
-    canActivate: [authGuard],
     loadComponent: () =>
       import('./home.component').then((component) => component.HomeComponent),
   },
--- src/app/guards/auth.guard.ts
+++
@@ -1,8 +0,0 @@
-import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
-import { AuthService } from '../services/auth.service';
-
-export const authGuard: CanActivateFn = async () => {
-  const authService = inject(AuthService);
-  return authService.hasValidSession();
-};
